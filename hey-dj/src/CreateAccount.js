93e5100262f368d0e51cb51a5e23eaf45beb365e
dblass613 <dcblassingame@aggies.ncat.edu>
2020-03-27 17:50:06 -0400

    beautification and added dashboard

--- hey-dj/src/CreateAccount.js
+++ hey-dj/src/CreateAccount.js
@@ -1,38 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './stylesheets/bootstrap.css';
-import './stylesheets/custom.css';
+import './Stylesheets/bootstrap.css';
+import './Stylesheets/createAccount.css';
+import Logo from './Images/HeyDJLogoWhite.png';
 
-
-function CreateAccount() {
-  return (
-    <div class="my-auto">
-        <div class="d-flex justify-content-center my-3 form-group">
-            <h1>Create an Account</h1>
-        </div>
-        <form>
-            <div class="my-3 form-group">
-                <label>Username</label>
-                <input type="text" name="" class="form-control" value="" placeholder="username"/>
+const CreateAccount = () =>
+      <div className = "body-wrapper">
+          <div className="d-flex justify-content-center my-3">
+            <div className= "Logo-container">
+              <img className= "Logo-img" src={Logo}/>
             </div>
-            <div class="my-3 form-group">
-                <label>Password</label>
-                <input type="password" name="" class="form-control" value="" placeholder="password"/>
-            </div>
-            <div class="my-3 form-group">
-                <label>First Name</label>
-                <input type="text" name="" class="form-control" value="" placeholder="John"/>
-            </div>
-            <div class="my-3 form-group">
-                <label>Last Name</label>
-                <input type="password" name="" class="form-control" value="" placeholder="Doe"/>
-            </div>
+          </div>
+          <hr className= "mbm" color = "#1B998B"></hr>
+          <div class="CreateAccount-form-container">
             <div class="d-flex justify-content-center my-3 form-group">
-                <button type="button" name="" class="btn">Create Account</button>
+                <h1>Create an Account</h1>
             </div>
-        </form>
+            <form>
+              <div class="my-3 form-group">
+                  <input type="text" name="" class="form-control" value="" placeholder="First Name"/>
+              </div>
+              <div class="my-3 form-group">
+                  <input type="password" name="" class="form-control" value="" placeholder="Last Name"/>
+              </div>
+              <div class="my-3 form-group">
+                  <input type="text" name="" class="form-control" value="" placeholder="Email"/>
+              </div>
+              <div class="my-3 form-group">
+                  <input type="text" name="" class="form-control" value="" placeholder="Username"/>
+              </div>
+              <div class="my-3 form-group">
+                  <input type="password" name="" class="form-control" value="" placeholder="Password"/>
+              </div>
+              <div class="d-flex justify-content-center my-3 form-group">
+                  <button type="button" name="" class="btn">Create Account</button>
+              </div>
+          </form>
+        </div>
     </div>
-  );
-}
-
 export default CreateAccount;
