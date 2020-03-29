aa3b5b509aa075c5413458c56bcc722e61e3af05
Joelle Banks <jlbanks@aggies.ncat.edu>
2020-03-29 14:42:28 -0400

    Revert "Update index.js"
    This reverts commit 7b987c2e0e1a4a8551c86fb157f37c84787e8d94.

--- hey-dj/src/index.js
+++ hey-dj/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Stylesheets/index.css';
+import './index.css';
 import App from './App';
 import Login from './Login'
 import * as serviceWorker from './serviceWorker';
