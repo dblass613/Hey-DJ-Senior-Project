8586d469a8150a44caf4e0e1f70f3cffde1a6b42
Joelle Banks <jlbanks@aggies.ncat.edu>
2020-04-20 23:40:44 -0400

    Organized Dashboard page
    CSS file and js file cleaned up. No visual changes made to the layout of the page.

--- hey-dj/src/Dashboard.js
+++ hey-dj/src/Dashboard.js
@@ -4,88 +4,68 @@ import './Stylesheets/dashboard.css';
 import Logo from './Images/HeyDJLogoWhite.png';
 
 function Dash(props) {
-  return (
-    <div class="container">
-            <div class="row justify-content-center my-3">
-                <div class="col d-flex justify-content-center">
-                    <button class="btn" name="">Host a Party</button>
-                </div>
-                <div class="col d-flex justify-content-center">
-                    <button class="btn" name="">Join a Party</button>
-                </div>
+    return (
+        <div class="container d-flex align-items-center flex-column">
+            <h1 class="my-3">My Parties</h1>
+            <div class="table-responsive my-3">
+                <table class="table table-dark table-bordered table-hover">
+                    <thead class="thead-light">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Party Name</th>
+                            <th scope="col">Date End</th>
+                            <th scope="col">Details</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>BDay Party</td>
+                            <td>Some Date</td>
+                            <td>Details</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Anniversary</td>
+                            <td>Some Date</td>
+                            <td>Details</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-            <div class="row justify-content-center my-3">
-                <h1>My Parties</h1>
-            </div>
-            <div class="row justify-content-center my-3">
-                <div class="table-responsive">
-                    <div class="col my-3">
-                        <table class="table table-dark table-bordered table-hover">
-                            <thead class="thead-light">
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Party Name</th>
-                                    <th scope="col">Date End</th>
-                                    <th scope="col">Details</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>BDay Party</td>
-                                    <td>Some Date</td>
-                                    <td>Details</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>Anniversary</td>
-                                    <td>Some Date</td>
-                                    <td>Details</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <div class="row justify-content-center my-3">
-                <h1>Joined Parties</h1>
-            </div>
-            <div class="row justify-content-center my-3">
-                <div class="table-responsive">
-                    <div class="col my-3">
-                        <table class="table table-dark table-bordered table-hover">
-                            <thead class="thead-light">
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Party Name</th>
-                                    <th scope="col">Date End</th>
-                                    <th scope="col">Details</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>BDay Party</td>
-                                    <td>Some Date</td>
-                                    <td>Details</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>Anniversary</td>
-                                    <td>Some Date</td>
-                                    <td>Details</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+            <h1 class="my-3">Joined Parties</h1>
+            <div class="table-responsive my-3">
+                <table class="table table-dark table-bordered table-hover">
+                    <thead class="thead-light">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Party Name</th>
+                            <th scope="col">Date End</th>
+                            <th scope="col">Details</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>BDay Party</td>
+                            <td>Some Date</td>
+                            <td>Details</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Anniversary</td>
+                            <td>Some Date</td>
+                            <td>Details</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
-);
+    );
 
 }
 
-const Dashboard= (props) =>
-  Dash(props);
-  
+const Dashboard = (props) =>
+    Dash(props);
+
 export default Dashboard;
